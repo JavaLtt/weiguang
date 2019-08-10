0e6ed1b0a6d41b8303254cae542b4384f80208db
JavaLtt <52741547+JavaLtt@users.noreply.github.com>
2019-08-10 20:48:36 +0800

    刘童Part第一次提交

--- src/main/java/com/weiguang/service/RoomService.java
+++ src/main/java/com/weiguang/service/RoomService.java
@@ -7,4 +7,10 @@ import java.util.Map;
 
 public interface RoomService{
     public PageInfo<Room> findallroom(int index, int size, Map map);
+    
+    //创建房间:搜索,条件查询房间
+    PageInfo<Room> selectAll(int index, int size, Room room);
+    //根据资源拥有着查询资源信息
+    Room selectByOwnerid(int ownerid);
+    
 }
