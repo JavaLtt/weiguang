0b981a495b72fd0e234557a9a0c08f10dee0dbc5
JavaLtt <52741547+JavaLtt@users.noreply.github.com>
2019-08-10 20:40:29 +0800

    刘童Part第一次提交

--- src/main/java/com/weiguang/mapper/RoomMapper.java
+++ src/main/java/com/weiguang/mapper/RoomMapper.java
@@ -6,6 +6,12 @@ import java.util.List;
 import java.util.Map;
 
 public interface RoomMapper {
+    
+     //创建房间,搜索,条件查询
+    List<Room> selectAll(Room room);
+    //根据资源拥有着查询资源信息
+    Room selectByOwnerid(int ownerid);
+    
     int deleteByPrimaryKey(Integer roomid);
 
     int insert(Room record);
