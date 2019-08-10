312444ea354c1cbb7bc9f96f40d776d681bb46cc
JavaLtt <52741547+JavaLtt@users.noreply.github.com>
2019-08-10 20:41:27 +0800

    刘童Part第一次提交

--- src/main/java/com/weiguang/mapper/FriendMapper.java
+++ src/main/java/com/weiguang/mapper/FriendMapper.java
@@ -3,6 +3,10 @@ package com.weiguang.mapper;
 import com.weiguang.pojo.Friend;
 
 public interface FriendMapper {
+    
+    //查询当前用户的全部比心好友的id
+    List<Friend> selectByUid(int uid);
+    
     int deleteByPrimaryKey(Integer id);
 
     int insert(Friend record);
