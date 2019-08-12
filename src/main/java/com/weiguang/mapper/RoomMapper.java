2cd23a583f10baeeeed4bb88720cfa726a307766
KawaiIN <39559575+KawaiIN@users.noreply.github.com>
2019-08-12 09:37:45 +0800

    update
    by KawaiIN

--- src/main/java/com/weiguang/mapper/RoomMapper.java
+++ src/main/java/com/weiguang/mapper/RoomMapper.java
@@ -7,6 +7,18 @@ import java.util.Map;
 
 public interface RoomMapper {
     
+    //通过关键字搜索视频
+    List selectVideosByName(String keyWord);
+
+    //关键字搜索视频数量
+    int selectVideoCount(String keyWord);
+
+    //通过关键字搜索音乐
+    List selectMusicByName(String keyWord);
+
+    //关键字搜索音乐数量
+    int selectMusicCount(String keyWord);
+    
      //创建房间,搜索,条件查询
     List<Room> selectAll(Room room);
     //根据资源拥有着查询资源信息
