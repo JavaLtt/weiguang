492533f34c56d1ad0f7c06b7f9531937ba0b3f2c
vk6dynasty <52741186+vk6dynasty@users.noreply.github.com>
2019-08-12 10:35:08 +0800

    Add files via upload

--- src/main/java/com/weiguang/mapper/ReplyMapper.java
+++ src/main/java/com/weiguang/mapper/ReplyMapper.java
@@ -1,8 +1,16 @@
 package com.weiguang.mapper;
 
 import com.weiguang.pojo.Reply;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface ReplyMapper {
+
+    //获取回复列表
+    List<Reply> getReplyList(Integer blogid);
+
+    List<Reply> getReplyByUid(@Param("uid")Integer uid);
     int deleteByPrimaryKey(Integer id);
 
     int insert(Reply record);
