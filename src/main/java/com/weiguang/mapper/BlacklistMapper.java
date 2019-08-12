35bb7f6cc82a226740826ad16e5dee5dfa0ba392
KawaiIN <39559575+KawaiIN@users.noreply.github.com>
2019-08-12 09:33:00 +0800

    update
    by KawaiIN

--- src/main/java/com/weiguang/mapper/BlacklistMapper.java
+++ src/main/java/com/weiguang/mapper/BlacklistMapper.java
@@ -3,6 +3,10 @@ package com.weiguang.mapper;
 import com.weiguang.pojo.Blacklist;
 
 public interface BlacklistMapper {
+    
+    //查找本人黑名单用户
+    List selectBlackByID(Integer id);
+    
     int deleteByPrimaryKey(Integer id);
 
     int insert(Blacklist record);
